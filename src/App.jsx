d420e1484373a215793a9e839ca694f5ecde4ba1
Cser Tibor <tibor.cser@attrecto.com>
2025-05-18 12:07:02 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen min-w-screen h-full w-full flex items-center justify-center bg-gray-700">
+      <h1 className='text-3xl font-bold underline text-white'>
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
