295c22f6f12854e0d542aa4961bd17507946efec
Varga Bertalan <borszay@gmail.com>
2026-01-12 14:13:37 +0100

    Input mező inkrementálása

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -5,20 +5,61 @@ import { FaRegTrashAlt } from "react-icons/fa";
 
 function Movies() {
   const [movieList, setMovieList] = useState(MOVIES);
+  const [editingID, setEditingID] = useState(null);
+  const [tempTitle, setTempTitle] = useState("");
 
   function handleRemove(id) {
     setMovieList(movieList.filter((movie) => movie.id !== id));
+  }
+
+  function handleSave(id) {
+    const updatedList = movieList.map((movie) =>
+      movie.id === id ? { ...movie, title: tempTitle } : movie
+    );
+    setMovieList(updatedList);
+    setEditingID(null);
   }
 
   return (
     <div>
       <ul>
         {movieList.map((movie) => (
-          <li key={movie.id}>
-            {movie.title}
-            <button onClick={() => handleRemove(movie.id)}>
-              <FaRegTrashAlt />
-            </button>
+          <li
+            key={movie.id}
+            className={editingID === movie.id ? "bg-yellow-100" : ""}
+          >
+            {editingID !== movie.id && <span>{movie.title}</span>}
+
+            {editingID === movie.id ? (
+              <div>
+                <input
+                  type="text"
+                  value={tempTitle}
+                  onChange={(e) => setTempTitle(e.target.value)}
+                />
+                <button
+                  disabled={tempTitle.trim() === ""}
+                  onClick={() => handleSave(movie.id)}
+                >
+                  Save
+                </button>
+                <button onClick={() => setEditingID(null)}>Cancel</button>
+              </div>
+            ) : (
+              <>
+                <button onClick={() => handleRemove(movie.id)}>
+                  <FaRegTrashAlt />
+                </button>
+                <button
+                  onClick={() => {
+                    setEditingID(movie.id);
+                    setTempTitle(movie.title);
+                  }}
+                >
+                  Szerkesztés
+                </button>
+              </>
+            )}
           </li>
         ))}
       </ul>
