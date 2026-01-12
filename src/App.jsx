80a51ed0437398967f0112e1c5b1deb82cf67ae4
Varga Bertalan <borszay@gmail.com>
2026-01-12 08:26:26 +0100

    Routing kialakítása, Navbar és Sandbox komponens elkészítése

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from "react";
-import Button from "./components/Button";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sandbox from "./pages/Sandbox.jsx";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handleDecrease() {
-    if (counter > 0) {
-      setCounter(counter - 1);
-    }
-  }
-  function handleIncrease() {
-    setCounter(counter + 1);
-  }
-  function handleReset() {
-    setCounter(0);
-  }
-
   return (
-    <div className="min-h-screen min-w-screen h-full w-full flex items-center justify-center bg-gray-700">
-      <h1 className="text-3xl font-bold underline text-white">
-        {/* Hello world! */}
-        {counter}
-      </h1>
-      <Button
-        text={"Decrease"}
-        onClick={handleDecrease}
-        disabled={counter < 1}
-      />
-      <Button text={"Increase"} onClick={handleIncrease} />
-      <Button text={"Reset"} onClick={handleReset} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Sandbox />} />
+        <Route path="sandbox" element={<Sandbox />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Sandbox.jsx
@@ -0,0 +1,35 @@
+import Button from "../components/Button"
+import { useState } from "react";
+
+function Sandbox() {
+  const [counter, setCounter] = useState(0);
+
+  function handleDecrease() {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  }
+  function handleIncrease() {
+    setCounter(counter + 1);
+  }
+  function handleReset() {
+    setCounter(0);
+  }
+  return (
+    <div className="min-h-screen min-w-screen h-full w-full flex items-center justify-center bg-gray-700">
+      <h1 className="text-3xl font-bold underline text-white">
+        {/* Hello world! */}
+        {counter}
+      </h1>
+      <Button
+        text={"Decrease"}
+        onClick={handleDecrease}
+        disabled={counter < 1}
+      />
+      <Button text={"Increase"} onClick={handleIncrease} />
+      <Button text={"Reset"} onClick={handleReset} />
+    </div>
+  );
+}
+
+export default Sandbox;
